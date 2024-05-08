793a9ea0d229bbd48c1a061e74a6717ee78dd2e2
psychoysy-1 <2860108595@qq.com>
2024-05-08 19:04:47 +0800

    完成天气

--- src/views/home/components/WeatherPart.vue
+++ src/views/home/components/WeatherPart.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref, onMounted } from 'vue'
+import { LocationFilled, Odometer, WindPower } from '@element-plus/icons-vue'
 
 // 定义 API 请求地址和参数
 const apiUrl = 'https://restapi.amap.com/v3/weather/weatherInfo'
@@ -26,10 +27,33 @@ onMounted(async () => {
 
 <template>
   <div class="weather" v-if="weather.city">
-    <div class="time">更新时间: {{ weather.reporttime }}</div>
-    <div class="main">{{ weather.weather }}</div>
+    <div class="address">
+      <el-icon><LocationFilled /></el-icon>
+      <span>{{ weather.province }}省 {{ weather.city }}</span>
+    </div>
+    <div class="main">
+      <span>{{ weather.temperature }}°C</span>
+      <span>{{ weather.weather }}</span>
+    </div>
     <div class="humidityAndWindpower">
-      湿度: {{ weather.humidity }}%, 风力: {{ weather.windpower }}
+      <div class="humidity">
+        <div class="icon">
+          <el-icon size="40"><Odometer /></el-icon>
+        </div>
+        <div class="text">
+          <span>{{ weather.humidity }}%</span>
+          <span>湿度</span>
+        </div>
+      </div>
+      <div class="windpower">
+        <div class="icon">
+          <el-icon size="40"><WindPower /></el-icon>
+        </div>
+        <div class="text">
+          <span>{{ weather.windpower }}Km/h</span>
+          <span>风力</span>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -38,16 +62,61 @@ onMounted(async () => {
 .weather {
   width: 16vw;
   height: 49vh;
-  background: linear-gradient(to bottom right, #9adaaf, #bf96da);
+  background-image: url('@/assets/warm-bg.jpg');
   padding: 1rem;
   box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 }
 
-.time,
-.main,
-.humidityAndWindpower {
-  color: white;
+/* 顶部 */
+.address {
+  width: 100%;
+  font-size: 20px;
   font-weight: bold;
-  margin-bottom: 0.5rem;
+}
+/* 中间温度和天气 */
+.main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-weight: bold;
+}
+.main span:first-child {
+  font-size: 70px;
+  font-family: 'Times New Roman', Times, serif;
+  margin-bottom: 10px;
+}
+.main span:last-child {
+  font-size: 25px;
+}
+
+/* 底部 */
+.humidityAndWindpower {
+  display: flex;
+  color: rgb(0, 0, 0);
+  display: flex;
+  justify-content: space-between;
+}
+.humidity,
+.windpower {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.icon {
+  margin: 0 10px;
+}
+
+.text {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+
+.text span {
+  font-size: 14px;
 }
 </style>
